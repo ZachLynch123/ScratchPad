8faa220bc4315e35cf62015bbb751a0ebfacaf5e
Unknown <Zachary.Lynch1337@gmail.com>
2018-01-31 19:45:25 -0800

    Took out unused code

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,9 @@
-import java.io.Console;
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		String a = "AAAAH";
+		System.out.println("I love Alli so much!\n");
 		System.out.println(a);
 	}
 
